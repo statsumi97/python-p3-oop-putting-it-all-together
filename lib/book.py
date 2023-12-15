4a6428fb59c31b28ecdab3d7c832772fda730aa7
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-15 12:37:50 -0500

    final product

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
+#Book in book.py has the title and page_count passed into __init__, and values can be set to new instance.
+#Book in book.py prints "page_count must be an integer" if page_count is not an integer.
+#Book in book.py outputs "Flipping the page...wow, you read fast!" when method turn_page() is called
+
 class Book:
-    pass
+    def __init__(self, title=None, page_count=0):
+        self.title = title
+        self.page_count = page_count
+    
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
+#Shoe in shoe.py has the brand and size passed to __init__, and values can be set to new instance.
+#Shoe in shoe.py prints "size must be an integer" if size is not an integer.
+#Shoe in shoe.py says that the shoe has been repaired.
+#Shoe in shoe.py creates an attribute on the instance called 'condition' and set equal to 'New' after repair.
+
 class Shoe:
-    pass
+    condition = 'New'
+
+    def __init__(self, brand=None, size=0):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size (self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
